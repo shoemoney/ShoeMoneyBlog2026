7fe83064725efd7e04db3a1ae30d6fb20d2b3e63
Jeremy Schoemaker <jeremy@shoemoney.com>
2026-01-25 05:51:14 -0600

    feat(07-01): add ClearsResponseCache trait to Post and Comment
    - Post model now clears response cache on create/update/delete
    - Comment model now clears response cache on create/update/delete
    - Ensures visitors see fresh content after any content changes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\ClearsResponseCache;
 use App\Services\ShortcodeProcessor;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -14,6 +15,7 @@ use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
+    use ClearsResponseCache;
     use HasFactory;
     use Searchable;
 
