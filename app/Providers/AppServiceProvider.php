1ac10b0b6a61ed7a3580e596b5396bc1158a15f2
Jeremy Schoemaker <jeremy@shoemoney.com>
2026-03-25 17:24:52 -0500

    Force HTTPS URLs in production for sitemap and all generated links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS and correct host when behind Nginx proxy
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+            URL::forceRootUrl(config('app.url'));
+        }
+
         $this->configureSeo();
         $this->configureAuthorization();
     }
